648ec23b5cb9e0b6de679a93f3475f4a530a386c
Laura LOspitao <lauralospitao@gmail.com>
2020-07-13 12:23:33 +0200

    #6 Set diferent modes in CodeMirror based IDE
    PHP as well as html mark-up set

--- codeeditor.php
+++ codeeditor.php
@@ -16,8 +16,20 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'
      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
      <script type="text/javascript" src="js/default.js"></script>
      <link rel="stylesheet" href="plugins/codemirror/lib/codemirror.css">
+     <!--Language Plugin-->
+     <script src="plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
      <script src="plugins/codemirror/mode/xml/xml.js"></script>
+     <script src="plugins/codemirror/mode/javascript/javascript.js"></script>
+     <script src="plugins/codemirror/mode/css/css.js"></script>
+     <script src="plugins/codemirror/mode/clike/clike.js"></script>
+     <script src="plugins/codemirror/mode/php/php.js"></script>
+     <!--Closetag plugin-->
      <script src="plugins/codemirror/addon/edit/closetag.js"></script>
+     <!--Autocomplete plugins-->
+     <script src="plugins/codemirror/addon/hint/show-hint.js"></script>
+     <script src="plugins/codemirror/addon/hint/css-hint.js"></script>
+     <link rel="stylesheet" href="plugins/codemirror/addon/hint/show-hint.css">
+     <!--Theme plugins-->
      <link rel="stylesheet" href="plugins/codemirror/theme/eclipse.css">
 
  </head>
--- js/default.js
+++ js/default.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
     var code = $(".codemirror-textarea")[0];
     var editor = CodeMirror.fromTextArea(code, {
         lineNumbers: true,
-        mode: "xml",
+        mode: "php",
+        mime: "application/x-httpd-php",
+        extraKeys: {"Ctrl-Space": "autocomplete",}
     });
     //when form submitted
     $("#preview-form").submit(function(e) {
